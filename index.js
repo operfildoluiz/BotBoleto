2b5b01b124c77367ab6db33d9ff809b22e3f3c83
Guilherme Santiago <guilherme.santiago95@gmail.com>
2019-03-28 17:07:18 -0300

    Take advantage of asynchronous feature of JS

--- index.js
+++ index.js
@@ -4,45 +4,46 @@ const path = require('path'),
       ora = require('ora'),
       currentPath = path.resolve(__dirname, 'boletos')
 
-const scrappers = {
-    unimed: require('./scrappers/unimed'),
-    copel: require('./scrappers/copel'),
-    ultragaz: require('./scrappers/ultragaz'),
-    das: require('./scrappers/das'),
-}
+const services = [
+    {
+        name: 'Unimed',
+        scrapper: require('./scrappers/unimed')
+    },
+    {
+        name: 'Copel',
+        scrapper: require('./scrappers/copel'),
+        params: []
+    },
+    {
+        name: 'Ultragaz',
+        scrapper: require('./scrappers/ultragaz'),
+        params: []
+    },
+    {
+        name: 'DASMEIBot1',
+        scrapper: require('./scrappers/das'),
+        params: [
+            process.env.DAS_CNPJ1
+        ]
+    },
+    {
+        name: 'DASMEIBot2',
+        scrapper: require('./scrappers/das'),
+        params: [
+            process.env.DAS_CNPJ2
+        ]
+    }
+]
 
 async function start() {
+    const promises = services.map(async service => {
+        const spinner = ora(`Iniciando ${service.name}`).start()
+        await service.scrapper(currentPath, spinner, ...service.params)
+        spinner.text = '${service.name} finalizado!'
+        spinner.succed()
+    })
 
-    let spinner;
-
-    spinner = ora('Iniciando o BotBoleto').start();
-    spinner.succeed()
-
-    spinner = ora('Iniciando o UnimedBot').start();
-    spinner = await scrappers.unimed(currentPath, spinner)
-    spinner.text = 'UnimedBot finalizado!';
-    spinner.succeed()
-
-    spinner = ora('Iniciando o CopelBot').start();
-    spinner = await scrappers.copel(currentPath, spinner)
-    spinner.text = 'CopelBot finalizado!';
-    spinner.succeed()
-
-    spinner = ora('Iniciando o UltragazBot').start();
-    spinner = await scrappers.ultragaz(currentPath, spinner)
-    spinner.text = 'UltragazBot finalizado!';
-    spinner.succeed()    
-
-    spinner = ora('Iniciando o DASMEIBot #1').start();
-    spinner = await scrappers.das(currentPath, spinner, process.env.DAS_CNPJ1)
-    spinner.text = 'DASMEIBot #1 finalizado!';
-    spinner.succeed()     
-
-    spinner = ora('Iniciando o DASMEIBot #2').start();
-    spinner = await scrappers.das(currentPath, spinner, process.env.DAS_CNPJ2)
-    spinner.text = 'DASMEIBot #2 finalizado!';
-    spinner.succeed()     
-
+    Promise.all(promises)
 }
 
 start()
